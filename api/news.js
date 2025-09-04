c35a05e96f1bca2bdba9c55c8a1e86ecd58f07af
AndriiRozanov <rozanovandriy88@gmail.com>
2025-09-04 13:23:21 +0300

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://newsapi.org/v2/everything?q=Trump&apiKey=YOUR_API_KEY', { next: { revalidate: 28800 } }); // 8 годин
+  const response = await fetch('https://newsapi.org/v2/everything?q=Trump&apiKey=YOUR_API_KEY', { next: { revalidate: 28800 } });
   const data = await response.json();
   const digest = data.articles.slice(0, 3).map(article => ({
     title: article.title,
